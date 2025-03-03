63163223f403407f2b3e7d35af221a9603e4ced1
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2025-03-03 22:14:32 +0000

    removing some test messages because it works (i think?)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const version = '1.0.4.4';
+const version = '1.0.4.5';
 const versionTitle = 'Establish Graph Visual Framework'
 
 
@@ -14,16 +14,9 @@ class FUNCTION {
     }
 
     evaluate(value) {
-        alert("We are evaluating")
         const variable = (this.functionVariable).toString();
-        alert(this);
-        alert(this.functionDefinition);
-        alert('test 1'+(this.functionDefinition).toString());
         let expression = ((this.functionDefinition).toString()).replace(variable, value.toString());
-        alert("test 2"+(expression).toString())
         expression = expression.replace('^', '**');
-        alert("test 3"+(expression).toString());
-        alert("test 4"+(eval(expression)).toString());
         return eval(expression);
     }
 
